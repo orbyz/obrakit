08b38d83da4edff8ba1660d014bfa8e6904196f5
Jonathan Olbes <olbes.es@gmail.com>
2026-06-29 10:51:32 +0200

    fix: nextConfig route Monorepo and warinings

--- apps/leads-web/src/app/layout.tsx
+++ apps/leads-web/src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <Analytics />
       <html
         lang="es"
+        data-scroll-behavior="smooth"
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
         <body className="min-h-full flex flex-col">{children}</body>
